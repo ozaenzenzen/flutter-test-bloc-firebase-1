e6621acb36f33b38583c68db0bd0d1f77d1fbddd
Fauzan Akmal Mahdi <recovery252@gmail.com>
2022-02-01 08:56:59 +0700

    update signin page

--- lib/page/signin_page.dart
+++ lib/page/signin_page.dart
@@ -1,3 +1,5 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -14,8 +16,17 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-  TextEditingController usernameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,71 +52,112 @@ class _SignInPageState extends State<SignInPage> {
             SizedBox(
               height: 30.h,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Username",
-                  style: GoogleFonts.poppins(
-                    fontSize: 14.sp,
-                  ),
-                ),
-                SizedBox(
-                  height: 5.h,
-                ),
-                SizedBox(
-                  height: 40.h,
-                  child: TextField(
-                    textAlignVertical: TextAlignVertical.center,
-                    controller: usernameController,
-                    decoration: InputDecoration(
-                      hintText: 'Username',
-                      hintStyle: GoogleFonts.poppins(
-                        fontSize: 12.sp,
-                      ),
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      hintText: "Email",
+                      border: OutlineInputBorder(),
                     ),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      return value != null && !EmailValidator.validate(value)
+                          ? 'Enter a valid email'
+                          : null;
+                    },
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 15.h,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Password",
-                  style: GoogleFonts.poppins(
-                    fontSize: 14.sp,
+                  const SizedBox(
+                    height: 10,
                   ),
-                ),
-                SizedBox(
-                  height: 5.h,
-                ),
-                SizedBox(
-                  height: 40.h,
-                  child: TextField(
-                    textAlignVertical: TextAlignVertical.center,
+                  TextFormField(
+                    keyboardType: TextInputType.text,
                     controller: passwordController,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: GoogleFonts.poppins(
-                        fontSize: 12.sp,
-                      ),
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      ),
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                      border: OutlineInputBorder(),
                     ),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      return value != null && value.length < 6
+                          ? "Enter min. 6 characters"
+                          : null;
+                    },
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 12,
+                  ),
+                ],
+              ),
             ),
+            // Column(
+            //   crossAxisAlignment: CrossAxisAlignment.start,
+            //   children: [
+            //     Text(
+            //       "Email",
+            //       style: GoogleFonts.poppins(
+            //         fontSize: 14.sp,
+            //       ),
+            //     ),
+            //     SizedBox(
+            //       height: 5.h,
+            //     ),
+            //     SizedBox(
+            //       height: 40.h,
+            //       child: TextField(
+            //         textAlignVertical: TextAlignVertical.center,
+            //         controller: emailController,
+            //         decoration: InputDecoration(
+            //           hintText: 'Email',
+            //           hintStyle: GoogleFonts.poppins(
+            //             fontSize: 12.sp,
+            //           ),
+            //           border: const OutlineInputBorder(
+            //             borderSide: BorderSide(),
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            // SizedBox(
+            //   height: 15.h,
+            // ),
+            // Column(
+            //   crossAxisAlignment: CrossAxisAlignment.start,
+            //   children: [
+            //     Text(
+            //       "Password",
+            //       style: GoogleFonts.poppins(
+            //         fontSize: 14.sp,
+            //       ),
+            //     ),
+            //     SizedBox(
+            //       height: 5.h,
+            //     ),
+            //     SizedBox(
+            //       height: 40.h,
+            //       child: TextField(
+            //         textAlignVertical: TextAlignVertical.center,
+            //         controller: passwordController,
+            //         decoration: InputDecoration(
+            //           hintText: 'Password',
+            //           hintStyle: GoogleFonts.poppins(
+            //             fontSize: 12.sp,
+            //           ),
+            //           border: const OutlineInputBorder(
+            //             borderSide: BorderSide(),
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
             SizedBox(
-              height: 30.h,
+              height: 20.h,
             ),
             BlocConsumer<AuthBloc, AuthState>(
               listener: (context, state) {
@@ -127,42 +179,107 @@ class _SignInPageState extends State<SignInPage> {
                   );
                 }
                 if (state is AuthUnauthenticated) {
-                  // return Container(
-                  //   alignment: Alignment.center,
-                  //   child: Text("Can't SignIn"),
-                  // );
-
-                  return SizedBox(
-                    width: 230.w,
-                    height: 40.h,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.cyan.shade600,
-                      ),
-                      onPressed: () {
-                        // Get.to(() => const MainPage());
-                        _authenticateWithGoogle(context);
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.network(
-                            "https://freesvg.org/img/1534129544.png",
-                            height: 30.h,
-                            width: 30.h,
+                  return Column(
+                    children: [
+                      SizedBox(
+                        width: 230.w,
+                        height: 30.h,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.cyan.shade600,
                           ),
-                          SizedBox(
-                            width: 20.w,
-                          ),
-                          Text(
-                            "Sign In With Google",
+                          onPressed: () {
+                            // Get.to(() => const MainPage());
+                            _authenticateWithEmailAndPassword(context);
+                          },
+                          child: Text(
+                            "Sign In",
                             style: GoogleFonts.poppins(
                               fontSize: 12.sp,
                             ),
                           ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          // Get.to(() => const MainPage());
+                          _authenticateWithGoogle(context);
+                        },
+                        child: Container(
+                          color: Colors.white,
+                          child: Image.network(
+                            "https://freesvg.org/img/1534129544.png",
+                            height: 30.h,
+                            width: 30.h,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Alredy have an account? ',
+                            style: GoogleFonts.poppins(
+                              fontSize: 11.sp,
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              //
+                              debugPrint("debugPrint");
+                            },
+                            child: Text(
+                              'Sign Up',
+                              style: GoogleFonts.poppins(
+                                fontSize: 11.sp,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                    ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      // SizedBox(
+                      //   width: 230.w,
+                      //   height: 40.h,
+                      //   child: ElevatedButton(
+                      //     style: ElevatedButton.styleFrom(
+                      //       primary: Colors.cyan.shade600,
+                      //     ),
+                      //     onPressed: () {
+                      //       // Get.to(() => const MainPage());
+                      //       _authenticateWithGoogle(context);
+                      //     },
+                      //     child: Row(
+                      //       mainAxisAlignment: MainAxisAlignment.center,
+                      //       children: [
+                      //         Image.network(
+                      //           "https://freesvg.org/img/1534129544.png",
+                      //           height: 30.h,
+                      //           width: 30.h,
+                      //         ),
+                      //         SizedBox(
+                      //           width: 20.w,
+                      //         ),
+                      //         Text(
+                      //           "Sign In With Google",
+                      //           style: GoogleFonts.poppins(
+                      //             fontSize: 12.sp,
+                      //           ),
+                      //         ),
+                      //       ],
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
                   );
                 }
                 return Container();
@@ -174,16 +291,16 @@ class _SignInPageState extends State<SignInPage> {
     );
   }
 
-  // void _authenticationWithEmailAndPassword(context) {
-  //   if (_formKey.currentState!.validate()) {
-  //     BlocProvider.of<AuthBloc>(context).add(
-  //       SignInRequested(
-  //         usernameController.text,
-  //         passwordController.text,
-  //       ),
-  //     );
-  //   }
-  // }
+  void _authenticateWithEmailAndPassword(context) {
+    if (_formKey.currentState!.validate()) {
+      BlocProvider.of<AuthBloc>(context).add(
+        SignInRequested(
+          emailController.text,
+          passwordController.text,
+        ),
+      );
+    }
+  }
 
   void _authenticateWithGoogle(context) {
     BlocProvider.of<AuthBloc>(context).add(
